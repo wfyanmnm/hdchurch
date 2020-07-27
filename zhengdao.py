fee5bd86f6b16e7b018880a9853e4bbed95b15d0
Fu Shikai <fushikai@qq.com>
2020-07-27 23:52:09 +0800

    Remove url and add some comments

--- zhengdao.py
+++ zhengdao.py
@@ -12,6 +12,10 @@ config=configparser.ConfigParser()
 config.read('config.ini')
 user = config['account']['User']
 password = config['account']['Password']
+# 文件上传接口
+url_file = config['account']['UrlFile']
+# 文章上传接口
+url_node = config['account']['UrlNode']
 
 while True:
     # 海淀教堂公众号的每日灵听或者主日礼拜链接地址
@@ -39,18 +43,18 @@ while True:
         mushi = re.findall(r'音视频\|(.*?)牧师', title)[0]
         
     print(mushi)
-    
-    
-    
+
+    # 日期和日期所在的月
     riqi = re.findall('",s="(.*?)";', html)[0][2:]
+    day = '20' + riqi
+    month = day[0:4]+day[5:7]
 
     zhengdaotimu = riqi + '|' + mushi +'：' + biaoti
     print(zhengdaotimu)  
 
+    # 从文章中提取所有图片并存成数组
     soup = BeautifulSoup(html, 'html.parser')
-
     imgs = soup.find_all("img", class_="rich_pages")
-
     images = []
     for img in imgs:
         images.append(img['data-src'])
@@ -67,15 +71,17 @@ while True:
     filename = riqi + ".png"
     im_resized.save(filename)
 
+    # 通过接口上传图片并获得图片id
     with open(filename, "rb") as image_file:
         filestring = base64.b64encode(image_file.read())
-    filepath = "public://sermon/202007/" + filename
-    url_file = 'http://www.hdchurch.org/hds/file.json'
+    filepath = "public://sermon/" + month + "/" + filename
+    
     fileimg = {'file[filepath]': filepath, 'file[file]': filestring, 'file[filename]': filename}
     r_file = requests.post(url_file, data=fileimg, auth=(user, password))
     result = json.loads(r_file.text)
     fid = result['fid']
 
+    # 证道的各个字段
     preacher = input("如果牧师名字错误，请重新输入：")
     if preacher == '':
         preacher = mushi
@@ -83,6 +89,7 @@ while True:
     if title_zhengdao == '':
         title_zhengdao = zhengdaotimu
     
+    # 从剪贴板获得证道经文
     body_true = 1
     while body_true:
         body_true = input("如果body内容已经复制，请直接回车，否则请输入0:")
@@ -91,7 +98,6 @@ while True:
     audio_link = input("请输入audio_link：")
     video_link = input("请输入video_link：")
 
-    url_node = 'http://www.hdchurch.org/hds/node'
     # 如果状态要设为未发布，则status设成None
     node = {'type':'sermon', 'title': title_zhengdao, "body":{"und":{"0":{"value": body}}}, "field_preacher":{"und": preacher}, "field_audio_link":{"und":{"0":{"url": audio_link}}}, "field_video_link":{"und":{"0":{"url": video_link}}}, "field_cover_image":{"und":{"0":{"fid": fid}}}, 'status': 1, 'promote': '1'}
     r = requests.post(url_node, json=node, auth=(user, password))
